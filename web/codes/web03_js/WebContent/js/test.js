636446e3c1a9b914da67b0051588e07e69c8d0a2
hyejinjeong9999 <hyejinj810@gmail.com>
2020-01-10 17:10:16 +0900

    Study 2020 01 10

---
+++ web/codes/web03_js/WebContent/js/test.js
@@ -0,0 +1,27 @@
+with(document){
+	write("javascript 외부파일로 저장하고 링크처리함.<br>");
+	write("javascript를 이용한 출력<br>");
+	write("javascript \"대소문자\" 구분한다.<br>");
+	write("c:\\hwp\\a.txt <br>");
+	writeln("<font color='red' size='5'>javascript's를 이용한 출력</font>");
+}
+//alert('안녕3333');
+
+//window.document.frm1.id.value="이도연";
+
+	
+function hello() {
+	var str = '점심 먹어요77777';
+//	console.log(str);
+//	alert('안녕2222  \n3333 \n6666 \n777  ' + str);
+	
+//	var name = document.getElementById("id").value;
+//	alert(name);
+	
+	alert(document.getElementById("id").value);
+}
+
+
+
+
+
