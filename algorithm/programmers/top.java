86de4e9a3714afe80819b64597560b264a4c5039
hyejin jeong <hyejinj810@gmail.com>
2020-07-19 23:42:18 +0900

    Study algorithm

--- algorithm/programmers/top.java
+++ algorithm/programmers/top.java
@@ -1,55 +1,55 @@
 //link : https://programmers.co.kr/learn/courses/30/lessons/42588
 
 
-class Solution {
-    public int[] solution(int[] heights){
-        int [] answer = new int[heights.length];
-        int num = heights.length-1;
+// class Solution {
+//     public int[] solution(int[] heights){
+//         int [] answer = new int[heights.length];
+//         int num = heights.length-1;
 
-        for(int i = heights.length-1; i>=1; i--){
+//         for(int i = heights.length-1; i>=1; i--){
 
-            for(int j = i-1; j>=0; j--){
-                if(heights[i]<heights[j]){
-                    answer[num--] = j+1;
-                    break;
-                }
-            }
+//             for(int j = i-1; j>=0; j--){
+//                 if(heights[i]<heights[j]){
+//                     answer[num--] = j+1;
+//                     break;
+//                 }
+//             }
             
         
-        }
+//         }
         
-        return answer; 
-    }
-}
+//         return answer; 
+//     }
+// }
 
-public class top{
-    public static void main(String[] args) {
+// public class top{
+//     public static void main(String[] args) {
 
-        int arr1 [] = {6,9,5,7,4};
-        int arr2 [] = {3,9,9,3,5,7,2};
-        int arr3 [] = {1,5,3,6,7,6,5};
+//         int arr1 [] = {6,9,5,7,4};
+//         int arr2 [] = {3,9,9,3,5,7,2};
+//         int arr3 [] = {1,5,3,6,7,6,5};
 
-        Solution s1 = new Solution();
-       int answer[] =  s1.solution(arr1);
-        int answer2[]  = s1.solution(arr2);
-        int answer3[] =  s1.solution(arr3);
+//         Solution s1 = new Solution();
+//        int answer[] =  s1.solution(arr1);
+//         int answer2[]  = s1.solution(arr2);
+//         int answer3[] =  s1.solution(arr3);
 
-      for(int i = 0; i< answer.length; i++){
-          System.out.println(answer[i]);
-      }
-      System.out.println("------------");
+//       for(int i = 0; i< answer.length; i++){
+//           System.out.println(answer[i]);
+//       }
+//       System.out.println("------------");
 
-      for(int i = 0; i< answer2.length; i++){
-        System.out.println(answer2[i]);
-    }
+//       for(int i = 0; i< answer2.length; i++){
+//         System.out.println(answer2[i]);
+//     }
 
-    System.out.println("------------");
+//     System.out.println("------------");
 
-    for(int i = 0; i< answer3.length; i++){
-        System.out.println(answer3[i]);
-    }
+//     for(int i = 0; i< answer3.length; i++){
+//         System.out.println(answer3[i]);
+//     }
 
         
-    }
+//     }
 
-}
+// }
---
+++ algorithm/programmers/탑.java
@@ -0,0 +1,39 @@
+//
+//https://programmers.co.kr/learn/courses/30/lessons/42588
+class Top {
+    public static int[] topSolution(int[] heights) {
+        int index = 0;
+        int[] answer = new int[heights.length];
+        // 54321 순으로 내려가기
+        // 5랑 4랑 비교해서 4가 더 값이 크면 answer에 추가
+        // 아니면 continue
+        for (int i = heights.length-1; i >= 1; i--) {
+            System.out.println("heights["+i+"] : " + heights[i]);
+            for(int j = i-1; j>0; j--){
+            System.out.println("heights["+j+"] : " + heights[j]);
+                if(heights[i]>heights[j]) continue;
+                else{
+                    answer[index] = heights[j];
+                }
+
+            }
+            
+            index++;
+        
+        }
+
+        return answer;
+    }
+
+    public static void main(String[] args) {
+        int[] top1 = { 6, 9, 5, 7, 4 };
+        int[] top2 = { 3, 9, 9, 3, 5, 7, 2 };
+        int[] top3 = { 1, 5, 3, 6, 7, 6, 5 };
+
+        int[] answer = topSolution(top1);
+        for (int i : answer) {
+            System.out.print(i + " ");
+        }
+
+    }
+}
